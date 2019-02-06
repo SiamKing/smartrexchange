c14aaee52c2991ba067ad3348731be056bc1986d
SiamKing <thesiameseelectric@gmail.com>
2019-02-06 09:28:37 -0800

    Added routes to pass first test for one_day route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'us_dollars#one_day'
+  get '/us_dollars/one_day', to: 'us_dollars#one_day', as: 'usd_one_day'
 end
