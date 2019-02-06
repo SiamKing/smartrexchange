622e603d7bc640289e3c672403d8e4d86dc0d590
SiamKing <thesiameseelectric@gmail.com>
2019-02-06 11:46:35 -0800

    Added webmock to spec_helper and created test for the api request

--- spec/requests/us_dollar_spec.rb
+++ spec/requests/us_dollar_spec.rb
@@ -6,5 +6,13 @@ RSpec.describe UsDollar, type: :request do
             get usd_one_day_path
             expect(response).to be_successful
         end
+
+        it "queries Cotacoes API for one day ratio" do
+            uri = URI('https://api.cotacoes.uol.com/currency/intraday/list/month?format=JSON&fields=askvalue,date&currency=1')
+
+            response = Net::HTTP.get(uri)
+
+            expect(response).to be_an_instance_of(String)
+        end
     end
 end
