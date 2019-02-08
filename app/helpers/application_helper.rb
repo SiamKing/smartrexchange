9eac98a098a3bc64e91269647ab6f2717ea04f88
SiamKing <thesiameseelectric@gmail.com>
2019-02-08 12:35:26 -0800

    Updated helper method to accept currency code parameter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
-    def api_call(type, period)
-        @resp = Faraday.get("https://api.cotacoes.uol.com/currency/#{type}/list#{period}?format=JSON&fields=askvalue,date&currency=1")
+    def api_call(type, period, currency)
+        @resp = Faraday.get("https://api.cotacoes.uol.com/currency/#{type}/list#{period}?format=JSON&fields=askvalue,date&currency=#{currency}")
 
         @request = JSON.parse(@resp.body)
         @request['docs'].inject({}) {|res, v| res[v['date']] = v['askvalue']; res}
