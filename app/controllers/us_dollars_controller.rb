10f423c124b773079f5e147b74272a1634b8dad7
SiamKing <thesiameseelectric@gmail.com>
2019-02-08 12:21:08 -0800

    Updated one_day and one_week actions to use api helper method. Refactored chart in views and moved shared code to helper method

--- app/controllers/us_dollars_controller.rb
+++ app/controllers/us_dollars_controller.rb
@@ -1,15 +1,11 @@
 class UsDollarsController < ApplicationController
 
     def one_day
-        @resp = Faraday.get('https://api.cotacoes.uol.com/currency/intraday/list?format=JSON&fields=askvalue,date&currency=1')
-
-        @request = JSON.parse(@resp.body)
+        @chart_data = helpers.api_call('intraday', '')
     end
 
     def one_week
-        @resp = Faraday.get('https://api.cotacoes.uol.com/currency/interday/list/week?format=JSON&fields=askvalue,date&currency=1')
-
-        @request = JSON.parse(@resp.body)
+        @chart_data = helpers.api_call('interday', '/week')
     end
     
     def one_month
