77dd9c57a92614a4a172b09cebfb6b6f7a646605
SiamKing <thesiameseelectric@gmail.com>
2019-02-07 16:25:09 -0800

    Added test for one_week route and view

--- spec/requests/us_dollar_spec.rb
+++ spec/requests/us_dollar_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe UsDollar, type: :feature do
     describe "request one day view" do
-        it "successfully renders one_day template" do
+        it "successfully renders one_day view" do
             visit usd_one_day_path
             expect(page).to have_css('#chart-1')
         end
@@ -15,5 +15,11 @@ RSpec.describe UsDollar, type: :feature do
 
             expect(response).to be_an_instance_of(String)
         end
+
+        it "successfully renders one_week view" do
+            visit usd_one_week_path
+            expect(page).to have_css('#chart-1')
+        end
+
     end
 end
