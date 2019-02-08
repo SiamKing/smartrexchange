84fabcc9e91a08bfcace6df7db2199b9c39c2853
SiamKing <thesiameseelectric@gmail.com>
2019-02-08 08:42:36 -0800

    Added route for usd one_month view

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'us_dollars#one_day'
   get '/us_dollars/one_day', to: 'us_dollars#one_day', as: 'usd_one_day'
   get '/us_dollars/one_week', to: 'us_dollars#one_week', as: 'usd_one_week'
+  get '/us_dollars/one_month', to: 'us_dollars#one_month', as: 'usd_one_month'
 end
