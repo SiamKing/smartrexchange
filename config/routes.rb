69d915d34dc551a86805dc0b1ec098c2ac977140
SiamKing <thesiameseelectric@gmail.com>
2019-02-08 09:29:56 -0800

    Added route for usd_three_month and link in one_month view for three_month view

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/us_dollars/one_day', to: 'us_dollars#one_day', as: 'usd_one_day'
   get '/us_dollars/one_week', to: 'us_dollars#one_week', as: 'usd_one_week'
   get '/us_dollars/one_month', to: 'us_dollars#one_month', as: 'usd_one_month'
+  get '/us_dollars/three_month', to: 'us_dollars#three_month', as: 'usd_three_month'
 end
