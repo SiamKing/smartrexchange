0eb63f9f6d6201c9862ac6ef220299e996024ffe
SiamKing <thesiameseelectric@gmail.com>
2019-02-09 13:49:47 -0800

    Added controller action for one_day and added currency codes constants to main controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     add_flash_types :success, :danger, :info
     
+    USD_CODE = '1'
+    EURO_CODE = '5'
+    AUD_CODE = '19'
     INTERDAY = "interday"
     INTRADAY = "intraday"
     WEEK = "/week"
--- app/controllers/euros_controller.rb
+++ app/controllers/euros_controller.rb
@@ -1,2 +1,13 @@
 class EurosController < ApplicationController
+
+    def one_day
+        resp = helpers.api_call(INTRADAY, '', EURO_CODE)
+        if resp.success?
+            @chart_data = helpers.json_parser(resp)
+        else
+            helpers.error_alert
+        end
+    end
+
+
 end
