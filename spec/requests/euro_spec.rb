681b3fbea92fa3d3f7beb781cc8611cb188af332
SiamKing <thesiameseelectric@gmail.com>
2019-02-09 14:51:26 -0800

    Added tests for one_week route/view

--- spec/requests/euro_spec.rb
+++ spec/requests/euro_spec.rb
@@ -12,5 +12,36 @@ RSpec.describe Euro, type: :request do
             expect(page).to have_no_css('#usd.active')
         end
 
+        it "routes succesfully to one_day view when link is clicked" do
+            visit euro_one_week_path
+            click_link "1 Day"
+
+            expect(page).to have_no_css('#euro-one-week.active')
+            expect(page).to have_css('#euro-one-day.active')
+            expect(page).to have_css('#euro.active')
+            expect(page).to have_no_css('#usd.active')
+        end
+
+    end
+
+    describe "one week euro view" do
+        it "successfully renders one_week view" do
+            visit euro_one_week_path
+
+            expect(page).to have_css('#chart-1')
+            expect(page).to have_css('#euro-one-week.active')
+            expect(page).to have_css('#euro.active')
+            expect(page).to have_no_css('#usd.active')
+        end
+
+        it "routes succesfully to one_week view when link is clicked" do
+            visit euro_one_day_path
+            click_link "1 Week"
+
+            expect(page).to have_no_css('#euro-one-day.active')
+            expect(page).to have_css('#euro-one-week.active')
+            expect(page).to have_css('#euro.active')
+        end
+
     end
 end
