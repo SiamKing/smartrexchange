c701b1c3d64b8e018e2e78f717e32efd927584f1
SiamKing <thesiameseelectric@gmail.com>
2019-02-09 13:51:51 -0800

    Added capybara dsl to config and modified euro one_day test

--- spec/requests/euro_spec.rb
+++ spec/requests/euro_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Euro, type: :request do
 
-    describe "request one day view" do
+    describe "one day euro view" do
         it "successfully renders one_day view" do
             visit euro_one_day_path
+
             expect(page).to have_css('#chart-1')
             expect(page).to have_css('#euro-one-day.active')
             expect(page).to have_css('#euro.active')
-            expect(page).to have_not_css('#usd.active')
+            expect(page).to have_no_css('#usd.active')
         end
 
     end
