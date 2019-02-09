68447496e796401b04a0b79d23b1ddffb7792cb7
SiamKing <thesiameseelectric@gmail.com>
2019-02-09 10:42:16 -0800

    Added flash types and security measure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    add_flash_types :success, :danger, :info
+    
     INTERDAY = "interday"
     INTRADAY = "intraday"
     WEEK = "/week"
