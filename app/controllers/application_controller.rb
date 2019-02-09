7b518d471d376fd0c6c7424b82e2b743afc795c9
SiamKing <thesiameseelectric@gmail.com>
2019-02-09 14:53:32 -0800

    Removed constants from application_controller and moved them to initializer file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     add_flash_types :success, :danger, :info
-    
-    USD_CODE = '1'
-    EURO_CODE = '5'
-    AUD_CODE = '19'
-    INTERDAY = "interday"
-    INTRADAY = "intraday"
-    WEEK = "/week"
-    MONTH = "/month"
-    MONTHS = "/months"
-    YEAR = "/year"
 end
