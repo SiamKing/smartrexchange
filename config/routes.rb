95a4c47fb8ad056777624cd256a20873530d3858
SiamKing <thesiameseelectric@gmail.com>
2019-02-09 12:37:08 -0800

    Updated root route to redirect...active_link_to was not working for one_day link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'us_dollars#one_day'
+  root to: redirect('/us_dollars/one_day')
   get '/us_dollars/one_day', to: 'us_dollars#one_day', as: 'usd_one_day'
   get '/us_dollars/one_week', to: 'us_dollars#one_week', as: 'usd_one_week'
   get '/us_dollars/one_month', to: 'us_dollars#one_month', as: 'usd_one_month'
