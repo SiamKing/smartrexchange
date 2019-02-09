bbbffb5f702b5d992eef33da6dae9e7e0a8ac1d4
SiamKing <thesiameseelectric@gmail.com>
2019-02-09 15:48:16 -0800

    Refactored and moved all common code in actions to view_handler method

--- app/controllers/euros_controller.rb
+++ app/controllers/euros_controller.rb
@@ -2,38 +2,31 @@ class EurosController < ApplicationController
 
     def one_day
         resp = helpers.api_call(INTRADAY, '', EURO_CODE)
-        if resp.success?
-            success_handler(resp)
-        else
-            helpers.error_alert
-        end
+        view_handler(resp)
     end
 
     def one_week
         resp = helpers.api_call(INTERDAY, WEEK, EURO_CODE)
-        if resp.success?
-            success_handler(resp)
-        else
-            helpers.error_alert
-        end
+        view_handler(resp)
     end
 
     def one_month
         resp = helpers.api_call(INTERDAY, MONTH, EURO_CODE)
-        if resp.success?
-            success_handler(resp)
-        else
-            helpers.error_alert
-        end
+        view_handler(resp)
     end
 
     private
 
-    def success_handler(resp)
-        @chart_data = helpers.json_parser(resp)
-        @legend = EURO
-        @controller = "euros"
-        @currency = "euro"
-        render 'shared/shared_view'
+    def view_handler(resp)
+        if resp.success?
+            @chart_data = helpers.json_parser(resp)
+            @legend = EURO
+            @controller = "euros"
+            @currency = "euro"
+            render 'shared/shared_view'
+        else
+            helpers.error_alert
+        end
+        
     end
 end
