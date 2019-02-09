99831a0920f38bad06e5ddf4004baf8937a231aa
SiamKing <thesiameseelectric@gmail.com>
2019-02-09 14:55:19 -0800

    Created action for one_week and DRYed up some common code for successful api calls and rendering

--- app/controllers/euros_controller.rb
+++ app/controllers/euros_controller.rb
@@ -3,11 +3,27 @@ class EurosController < ApplicationController
     def one_day
         resp = helpers.api_call(INTRADAY, '', EURO_CODE)
         if resp.success?
-            @chart_data = helpers.json_parser(resp)
+            success_handler(resp)
         else
             helpers.error_alert
         end
     end
 
+    def one_week
+        resp = helpers.api_call(INTERDAY, WEEK, EURO_CODE)
+        if resp.success?
+            success_handler(resp)
+        else
+            helpers.error_alert
+        end
+    end
 
+    private
+
+    def success_handler(resp)
+        @chart_data = helpers.json_parser(resp)
+        @legend = EURO
+        @controller = "euros"
+        render 'shared/shared_view'
+    end
 end
