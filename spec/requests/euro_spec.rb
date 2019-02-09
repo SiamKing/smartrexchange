e6dbf9d09769ffb01c0c0d0551c1a1431d544d43
SiamKing <thesiameseelectric@gmail.com>
2019-02-09 15:12:11 -0800

    Added test for one_month view

--- spec/requests/euro_spec.rb
+++ spec/requests/euro_spec.rb
@@ -44,4 +44,16 @@ RSpec.describe Euro, type: :request do
         end
 
     end
+    
+    describe "one month euro view" do
+        it "successfully renders one_month view" do
+            visit euro_one_month_path
+
+            expect(page).to have_css('#chart-1')
+            expect(page).to have_css('#euro-one-month.active')
+            expect(page).to have_css('#euro.active')
+            expect(page).to have_no_css('#usd.active')
+        end
+
+    end
 end
