acda2815700edde937737ae6778a005e920481fe
SiamKing <thesiameseelectric@gmail.com>
2019-02-09 16:55:46 -0800

    Moved view_handler method so it is available to all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     add_flash_types :success, :danger, :info
+
+    def view_handler(resp, legend, controller)
+        if resp.success?
+            @chart_data = helpers.json_parser(resp)
+            @legend = legend
+            @controller = controller
+            @currency = legend.downcase
+            render 'shared/shared_view'
+        else
+            helpers.error_alert
+        end
+    end
+
 end
