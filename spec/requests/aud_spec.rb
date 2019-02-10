9c8fa8bd0f93a09464d46492939fe6fdd189ad39
SiamKing <thesiameseelectric@gmail.com>
2019-02-09 17:17:24 -0800

    Updated tests for aud

--- spec/requests/aud_spec.rb
+++ spec/requests/aud_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe Euro, type: :request do
+RSpec.describe AUD, type: :request do
 
-    describe "one day euro view" do
+    describe "one day AUD view" do
         it "successfully renders one_day view" do
-            visit euro_one_day_path
+            visit aud_one_day_path
 
             expect(page).to have_css('#chart-1')
-            expect(page).to have_css('#euro-one-day.active')
-            expect(page).to have_css('#euro.active')
+            expect(page).to have_css('#aud-one-day.active')
+            expect(page).to have_css('#aud.active')
             expect(page).to have_no_css('#usd.active')
         end
 
         it "routes succesfully to one_day view when link is clicked" do
-            visit euro_one_week_path
+            visit aud_one_week_path
             click_link "1 Day"
 
-            expect(page).to have_no_css('#euro-one-week.active')
-            expect(page).to have_css('#euro-one-day.active')
-            expect(page).to have_css('#euro.active')
+            expect(page).to have_no_css('#aud-one-week.active')
+            expect(page).to have_css('#aud-one-day.active')
+            expect(page).to have_css('#aud.active')
             expect(page).to have_no_css('#usd.active')
         end
 
