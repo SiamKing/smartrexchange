dddffe18488b76c1189588fed8b9a036d17c4346
SiamKing <thesiameseelectric@gmail.com>
2019-02-09 17:13:19 -0800

    Generated auds controller and associated files with first test

---
+++ app/controllers/auds_controller.rb
@@ -0,0 +1,2 @@
+class AudsController < ApplicationController
+end
---
+++ spec/requests/aud_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Euro, type: :request do
+
+    describe "one day euro view" do
+        it "successfully renders one_day view" do
+            visit euro_one_day_path
+
+            expect(page).to have_css('#chart-1')
+            expect(page).to have_css('#euro-one-day.active')
+            expect(page).to have_css('#euro.active')
+            expect(page).to have_no_css('#usd.active')
+        end
+
+        it "routes succesfully to one_day view when link is clicked" do
+            visit euro_one_week_path
+            click_link "1 Day"
+
+            expect(page).to have_no_css('#euro-one-week.active')
+            expect(page).to have_css('#euro-one-day.active')
+            expect(page).to have_css('#euro.active')
+            expect(page).to have_no_css('#usd.active')
+        end
+
+    end
+
+end
