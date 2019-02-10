6aa60e5e81ff95862884c988a07db7d0ce2abd4c
SiamKing <thesiameseelectric@gmail.com>
2019-02-09 17:28:47 -0800

    Created one_day action

--- app/controllers/auds_controller.rb
+++ app/controllers/auds_controller.rb
@@ -1,2 +1,7 @@
 class AudsController < ApplicationController
+
+    def one_day
+        resp = helpers.api_call(INTRADAY, '', AUD_CODE)
+        view_handler(resp, AUD, AUD_CONTROLLER)
+    end
 end
