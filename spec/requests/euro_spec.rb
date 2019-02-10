15815436d1f1a2a47df18288068f6274905c2a52
SiamKing <thesiameseelectric@gmail.com>
2019-02-09 16:23:28 -0800

    Added test for one year route/view

--- spec/requests/euro_spec.rb
+++ spec/requests/euro_spec.rb
@@ -86,4 +86,16 @@ RSpec.describe Euro, type: :request do
         end
 
     end
+
+    describe "one year euro view" do
+        it "successfully renders one year view" do
+            visit euro_one_year_path
+
+            expect(page).to have_css('#chart-1')
+            expect(page).to have_css('#euro-one-year.active')
+            expect(page).to have_css('#euro.active')
+            expect(page).to have_no_css('#usd.active')
+        end
+
+    end
 end
