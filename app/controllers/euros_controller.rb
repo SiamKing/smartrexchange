8ef0dcb993fca3491eaada183f803888e1afb9f3
SiamKing <thesiameseelectric@gmail.com>
2019-02-09 17:02:44 -0800

    Refactored all actions to use common method in main controller to handle view

--- app/controllers/euros_controller.rb
+++ app/controllers/euros_controller.rb
@@ -2,41 +2,27 @@ class EurosController < ApplicationController
 
     def one_day
         resp = helpers.api_call(INTRADAY, '', EURO_CODE)
-        view_handler(resp)
+        view_handler(resp, EURO, EURO_CONTROLLER)
     end
 
     def one_week
         resp = helpers.api_call(INTERDAY, WEEK, EURO_CODE)
-        view_handler(resp)
+        view_handler(resp, EURO, EURO_CONTROLLER)
     end
 
     def one_month
         resp = helpers.api_call(INTERDAY, MONTH, EURO_CODE)
-        view_handler(resp)
+        view_handler(resp, EURO, EURO_CONTROLLER)
     end
 
     def three_month
         resp = helpers.api_call(INTERDAY, MONTHS, EURO_CODE)
-        view_handler(resp)
+        view_handler(resp, EURO, EURO_CONTROLLER)
     end
     
     def one_year
         resp = helpers.api_call(INTERDAY, YEAR, EURO_CODE)
-        view_handler(resp)
+        view_handler(resp, EURO, EURO_CONTROLLER)
     end
 
-    private
-
-    def view_handler(resp)
-        if resp.success?
-            @chart_data = helpers.json_parser(resp)
-            @legend = EURO
-            @controller = "euros"
-            @currency = "euro"
-            render 'shared/shared_view'
-        else
-            helpers.error_alert
-        end
-        
-    end
 end
