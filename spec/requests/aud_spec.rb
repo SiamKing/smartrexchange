3d0a71b371459ab57d8a8978e43079a68dc5b7e7
SiamKing <thesiameseelectric@gmail.com>
2019-02-09 17:33:16 -0800

    Added tests for one week

--- spec/requests/aud_spec.rb
+++ spec/requests/aud_spec.rb
@@ -24,4 +24,26 @@ RSpec.describe AUD, type: :request do
 
     end
 
+    describe "one week AUD view" do
+        it "successfully renders one_week view" do
+            visit aud_one_week_path
+
+            expect(page).to have_css('#chart-1')
+            expect(page).to have_css('#aud-one-week.active')
+            expect(page).to have_css('#aud.active')
+            expect(page).to have_no_css('#usd.active')
+        end
+
+        it "routes succesfully to one_week view when link is clicked" do
+            visit aud_one_day_path
+            click_link "1 Week"
+
+            expect(page).to have_no_css('#aud-one-day.active')
+            expect(page).to have_css('#aud-one-week.active')
+            expect(page).to have_css('#aud.active')
+            expect(page).to have_no_css('#usd.active')
+        end
+
+    end
+
 end
