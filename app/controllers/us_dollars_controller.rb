caee05c2f9a440d384f06d25c29b86e65941bfd7
SiamKing <thesiameseelectric@gmail.com>
2019-02-09 16:54:54 -0800

    Moved view_handler to main controller so it can be used in every controller. Refactored usd controller actions to use it

--- app/controllers/us_dollars_controller.rb
+++ app/controllers/us_dollars_controller.rb
@@ -1,49 +1,28 @@
 class UsDollarsController < ApplicationController
-    CURRENCY_CODE = '1'
 
     def one_day
-        resp = helpers.api_call(INTRADAY, '', CURRENCY_CODE)
-        if resp.success?
-            @chart_data = helpers.json_parser(resp)
-        else
-            helpers.error_alert
-        end
+        resp = helpers.api_call(INTRADAY, '', USD_CODE)
+        view_handler(resp, USD, USD_CONTROLLER)
     end
 
     def one_week
-        resp = helpers.api_call(INTERDAY, WEEK, CURRENCY_CODE)
-        if resp.success?
-            @chart_data = helpers.json_parser(resp)
-        else
-            helpers.error_alert
-        end
+        resp = helpers.api_call(INTERDAY, WEEK, USD_CODE)
+        view_handler(resp, USD, USD_CONTROLLER)
     end
-    
+
     def one_month
-        resp = helpers.api_call(INTERDAY, MONTH, CURRENCY_CODE)
-        if resp.success?
-            @chart_data = helpers.json_parser(resp)
-        else
-            helpers.error_alert
-        end
-        
+        resp = helpers.api_call(INTERDAY, MONTH, USD_CODE)
+        view_handler(resp, USD, USD_CONTROLLER)
     end
 
     def three_month
-        resp = helpers.api_call(INTERDAY, MONTHS, CURRENCY_CODE)
-        if resp.success?
-            @chart_data = helpers.json_parser(resp)
-        else
-            helpers.error_alert
-        end
+        resp = helpers.api_call(INTERDAY, MONTHS, USD_CODE)
+        view_handler(resp, USD, USD_CONTROLLER)
     end
     
     def one_year
-        resp = helpers.api_call(INTERDAY, YEAR, CURRENCY_CODE)
-        if resp.success?
-            @chart_data = helpers.json_parser(resp)
-        else
-            helpers.error_alert
-        end
+        resp = helpers.api_call(INTERDAY, YEAR, USD_CODE)
+        view_handler(resp, USD, USD_CONTROLLER)
     end
+
 end
