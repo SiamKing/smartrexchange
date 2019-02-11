136d9078d505c90c265b60c873b802df98bdbc17
SiamKing <thesiameseelectric@gmail.com>
2019-02-11 11:28:11 -0800

    Refactored all usd tests to use new helpers

--- spec/requests/us_dollar_spec.rb
+++ spec/requests/us_dollar_spec.rb
@@ -13,82 +13,41 @@ RSpec.describe UsDollar, type: :request do
 
     describe "one week route/view" do
         it "successfully renders one_week view" do
-            visit usd_one_week_path
-            expect(page).to have_css('#chart-1')
-            expect(page).to have_css('#usd-one-week.active')
-            expect(page).to have_css('#usd.active')
+            view_test("us_dollars", "usd", "one", "week", "aud")
         end
 
         it "routes to one_week view when link is clicked", js: true do
-            visit usd_one_day_path
-            click_link "1 Week"
-
-            expect(page).to have_no_css('#usd-one-day.active')
-            expect(page).to have_css('#usd-one-week.active')
-            expect(page).to have_css('#usd.active')
+            view_link_test("us_dollars", "one_day", "1 Week", "one-day", "one-week", "usd", "aud")
         end
     end
 
     describe "one month route/view" do
         it "successfully renders one_month view" do
-            visit usd_one_month_path
-
-            expect(page).to have_css('#chart-1')
-            expect(page).to have_no_css('#usd-one-week.active')
-            expect(page).to have_css('#usd-one-month.active')
-            expect(page).to have_css('#usd.active')
+            view_test("us_dollars", "usd", "one", "month", "aud")
         end
 
         it "routes to one_month view when link is clicked", js: true do
-            visit usd_one_week_path
-            click_link "1 Month"
-
-            expect(page).to have_css('#chart-1')
-            expect(page).to have_no_css('#usd-one-week.active')
-            expect(page).to have_css('#usd-one-month.active')
-            expect(page).to have_css('#usd.active')
+            view_link_test("us_dollars", "one_week", "1 Month", "one-week", "one-month", "usd", "aud")
         end
     end
 
     describe "three month route/view" do
         it "successfully renders three_month view" do
-            visit usd_three_month_path
-
-            expect(page).to have_css('#chart-1')
-            expect(page).to have_no_css('#usd-one-month.active')
-            expect(page).to have_css('#usd-three-month.active')
-            expect(page).to have_css('#usd.active')
+            view_test("us_dollars", "usd", "three", "month", "aud")
         end
         
         it "routes to three_month view when link is clicked", js: true do
-            visit usd_one_month_path
-            click_link "3 Month"
-
-            expect(page).to have_css('#chart-1')
-            expect(page).to have_no_css('#usd-one-month.active')
-            expect(page).to have_css('#usd-three-month.active')
-            expect(page).to have_css('#usd.active')
+            view_link_test("us_dollars", "one_month", "3 Month", "one-month", "three-month", "usd", "aud")
         end
     end
 
     describe "one year route/view" do
         it "successfully renders one_year view" do
-            visit usd_one_year_path
-
-            expect(page).to have_css('#chart-1')
-            expect(page).to have_no_css('#usd-one-month.active')
-            expect(page).to have_css('#usd-one-year.active')
-            expect(page).to have_css('#usd.active')
+            view_test("us_dollars", "usd", "one", "year", "aud")
         end
         
         it "routes to one_year view when link is clicked", js: true do
-            visit usd_one_month_path
-            click_link "1 Year"
-
-            expect(page).to have_css('#chart-1')
-            expect(page).to have_no_css('#usd-three-month.active')
-            expect(page).to have_css('#usd-one-year.active')
-            expect(page).to have_css('#usd.active')
+            view_link_test("us_dollars", "three_month", "1 Year", "three-month", "one-year", "usd", "aud")
         end
 
     end
