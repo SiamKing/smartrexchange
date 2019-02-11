54bc9893c1fd6c9afdca6f03e9c1f37bde3f163d
SiamKing <thesiameseelectric@gmail.com>
2019-02-10 18:48:24 -0800

    Updated tests - some were not set up correctly in "describe" blocks - fixed...all passing

--- spec/requests/us_dollar_spec.rb
+++ spec/requests/us_dollar_spec.rb
@@ -9,6 +9,17 @@ RSpec.describe UsDollar, type: :request do
             expect(page).to have_css('#usd.active')
         end
 
+        it "routes to one_day view when link is clicked", js: true do
+            visit usd_one_week_path
+            click_link "1 Day"
+
+            expect(page).to have_no_css('#usd-one-week.active')
+            expect(page).to have_css('#usd-one-day.active')
+            expect(page).to have_css('#usd.active')
+        end
+    end
+
+    describe "one week route/view" do
         it "successfully renders one_week view" do
             visit usd_one_week_path
             expect(page).to have_css('#chart-1')
@@ -24,7 +35,9 @@ RSpec.describe UsDollar, type: :request do
             expect(page).to have_css('#usd-one-week.active')
             expect(page).to have_css('#usd.active')
         end
+    end
 
+    describe "one month route/view" do
         it "successfully renders one_month view" do
             visit usd_one_month_path
 
@@ -43,7 +56,9 @@ RSpec.describe UsDollar, type: :request do
             expect(page).to have_css('#usd-one-month.active')
             expect(page).to have_css('#usd.active')
         end
+    end
 
+    describe "three month route/view" do
         it "successfully renders three_month view" do
             visit usd_three_month_path
 
@@ -62,7 +77,9 @@ RSpec.describe UsDollar, type: :request do
             expect(page).to have_css('#usd-three-month.active')
             expect(page).to have_css('#usd.active')
         end
+    end
 
+    describe "one year route/view" do
         it "successfully renders one_year view" do
             visit usd_one_year_path
 
