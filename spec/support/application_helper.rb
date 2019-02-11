6e360fac1ed83bbba080074054beb6e26ff12d06
SiamKing <thesiameseelectric@gmail.com>
2019-02-11 10:06:35 -0800

    Updated arguments for helper test

--- spec/support/application_helper.rb
+++ spec/support/application_helper.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 module ApplicationHelper
 
-    def view_test(controller, currency, view, view_class, foreign_currency)
-        visit "/#{controller}/#{view}"
+    def view_test(controller, currency, number, time, foreign_currency)
+        visit "/#{controller}/#{number}_#{time}"
 
         expect(page).to have_css("#chart-1")
-        expect(page).to have_css("##{currency}-#{view_class}.active")
+        expect(page).to have_css("##{currency}-#{number}-#{time}.active")
         expect(page).to have_css("##{currency}.active")
         expect(page).to have_no_css("##{foreign_currency}.active")
     end
